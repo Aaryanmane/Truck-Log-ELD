55426b14ad4178f26bdce0bd8369d515d2aa03bf
Your Name <your-email@gmail.com>
2026-06-22 17:43:27 +0530

    fixing frontend img

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function MapView({ legs }) {
   const mapRef = useRef(null);
